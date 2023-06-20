ccb68e7a12c69d47ae92835c9f7ee1a0e3a618a6
Vincent826826 <vincent826826@gmail.com>
2023-06-20 15:38:08 +0800

    modify test

--- test.cu
+++ test.cu
@@ -3,32 +3,58 @@
 #include <cuda_runtime.h>
 using namespace std;
 
-const int BlockSize = 4;
-const int ThreadNum = 4;
-const int N = 4;
-__global__ void MatAdd(int *A)
-{
-   int i = threadIdx.x;
-   int j = threadIdx.y;
-   printf("A[%d][%d]=%d\n", i, j, A[i * N + j]));
-}
+const int BlockSize = 1;
+const int ThreadNum = 3;
+const int ARRAY_SIZE = 3;
 
+__global__ void test(int *d_oringinal, int*d_result)
+{
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    for(int i = 0; i < 8; i++)
+	{
+		d_result[8*idx + i] = d_oringinal[idx] + i;
+	}
+
+}
 
 int main()
 {
-	int **a = (int **) malloc (N * sizeof(int *));
-	for(int i=0; i < N;i++) 
-		a[i]= (int *) malloc (N * sizeof(int));
-	int *c_a;
-	for (int i = 0; i < N; i++) 
-		for(int j = 0; j < N; j++) 
-			a[i][j] = 1;
-	cudaMalloc((void **)&c_a, N * sizeof(int) * N);
-	cudaMemcpy(c_a, a, N * sizeof(int) * N, cudaMemcpyHostToDevice);
-
+    int oringinal[ARRAY_SIZE] = {10, 20, 30};
+	int *d_oringinal = 0;
+	int *result  = (int*)calloc(8*ARRAY_SIZE, sizeof(int));
+	int *d_result = 0;
+	int i;
+	
+	cout << "a[N] array before scaling: [";
+	for(i=0;i<ARRAY_SIZE;i++)
+	{
+		cout << oringinal[i] << " ";
+	}
+	cout << "]" << endl;
+	
+	cudaMalloc((void**) &d_oringinal, sizeof(int)*ARRAY_SIZE);
+	cudaMemcpy(d_oringinal, oringinal, sizeof(int)*ARRAY_SIZE, cudaMemcpyHostToDevice);
+	cudaMalloc((void**) &d_result, sizeof(int)*ARRAY_SIZE*8);
+	cudaMemcpy(d_result, result, sizeof(int)*ARRAY_SIZE*8, cudaMemcpyHostToDevice);
+	
     dim3 dimBlock(BlockSize);
     dim3 dimGrid(ThreadNum);
-    MatAdd<<<dimGrid,dimBlock>>>(c_a);
-
+    test<<<dimGrid,dimBlock>>>(d_oringinal, d_result);
+	cudaDeviceSynchronize();
+	
+	cudaMemcpy(result, d_result,sizeof(int)*ARRAY_SIZE*8,cudaMemcpyDeviceToHost);
+	
+	cout << "Result[N] array after scaling:"<<endl; 
+    cout<<"[";
+	for(i=0; i<ARRAY_SIZE; i++)
+	{
+		for(int j = 0; j < 8; j ++)
+		{
+			cout << result[8*i + j]<<",";
+		}
+        
+	}
+	cout << "]" << endl;
+	
 	return 0;
 }
